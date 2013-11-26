22f20504b222cd940373ff3ab306ab54e17599bc
Gennady Kovshenin <gennady@kovshenin.com>
2013-11-26 22:48:38 +0600

    added HTTP_USER_AGENT

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 	require 'includes/bootstrap.php';
+	$_SERVER['HTTP_USER_AGENT'] = '';
 	define( 'GF_DIGEST_DOING_TESTS', true );
 	require dirname( __FILE__ ) . '/../gravityforms-digest/gravityforms-digest.php';
 ?>
